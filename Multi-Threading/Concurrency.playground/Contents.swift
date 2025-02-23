1f6e330291b8d3d96d04ebf9222951f49614c0ea
Prashuk <prashuk.ajmera@gmail.com>
2025-02-22 22:39:30 -0800

    updated

--- Multi-Threading/Concurrency.playground/Contents.swift
+++ Multi-Threading/Concurrency.playground/Contents.swift
@@ -1,31 +1,33 @@
 import Foundation
-import PlaygroundSupport
 
-//PlaygroundPage.current.needsIndefiniteExecution = true
 
 /*
  Concurrency -
  Multiple instruction at same time, but one task at a time
  
  Parallelism -
- Multiple processing simultenously
+ Multiple processing simultaneously
  */
 
 /*
+ 
  Serial Queue -
- One taks at a time
+ One task at a time
  
  Concurrent Queue -
  Multiple task at a time
+ 
  */
 
 /*
+ 
  Manner of execution
  Synchronous -
  Block the execution till the task is completed
  
  Asynchronous -
- Don't block the current execution. Continue the execution of current task, while new task execure asynchronously
+ Don't block the current execution. Continue the execution of current task, while new task execute asynchronously
+ 
  */
 
 /*
@@ -35,31 +37,30 @@ import PlaygroundSupport
 
 // Main Queue
 var counter = 1
-DispatchQueue.main.async { // DispatchQueue.main is a serial queue, so once task a time, but executing in async way, will nto block the cu
+DispatchQueue.main.async { // DispatchQueue.main is a serial queue, so one task at a time, but executing in async way, will not block the current task
     for i in 0...3 {
         counter = i
         print("\(counter)")
     }
 }
-
 for i in 4...6 {
     counter = i
     print("\(counter)")
 }
-
 DispatchQueue.main.async {
     counter = 9
     print("\(counter)")
 }
 
-// Gloabl Queue
+
+// Global Queue
 DispatchQueue.main.async {
     print(Thread.isMainThread ? "A" :  "B")
 }
-
 DispatchQueue.global(qos: .userInteractive).async {
     print(Thread.isMainThread ? "C" : "D")
 }
+
 
 // QoS
 
